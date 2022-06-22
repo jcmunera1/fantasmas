e030e2298ed88d8d210ea29f0f248a9b53fd5f08
Juan Camilo Munera <jcmunera1@gmail.com>
2022-06-21 19:54:24 -0500

    commit prueba mbp

--- Fantasma.js
+++ Fantasma.js
@@ -1,6 +1,6 @@
-constructor(x,y){
-    if(new.targtet == Fantasma){
-       trow new Error("Sorry, this class should not be intantiated"); 
+constructor(x,y); {
+    if(new.target == Fantasma){
+       throw new Error("Sorry, this class should not be intantiated"); 
     }
     this.x = x;
     this.y = y;
@@ -9,10 +9,12 @@ constructor(x,y){
     this.g = 0;
     this.g = 0;
 }
-mostrar(){
+mostrar(); {
     fill(this.r, this.g, this,b);
     rectMode(CENTER);
     rect(this.x, this.y, 50, 50);
     rectMode(CORNER); 
     this.especial();       
 }
+
+// edit the object and update the state bla bla blacklist
